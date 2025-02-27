3de133a6323d3d7cbf418f82f9dd858546f79b85
Sunil <sunildivase07@gmail.com>
2025-02-27 20:32:24 +0530

    initial import

--- src/com/lambdastreamhealthcare/model/Hospital.java
+++ src/com/lambdastreamhealthcare/model/Hospital.java
@@ -1,5 +1,7 @@
 package com.lambdastreamhealthcare.model;
 
+import java.util.Objects;
+
 public class Hospital {
 
     private int hospitalId;
@@ -7,4 +9,68 @@ public class Hospital {
     private String hospitalAddress;
     private long contactNo;
     private String emailId;
+
+    public int getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(int hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getHospitalAddress() {
+        return hospitalAddress;
+    }
+
+    public void setHospitalAddress(String hospitalAddress) {
+        this.hospitalAddress = hospitalAddress;
+    }
+
+    public long getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(long contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital{" +
+                "hospitalId=" + hospitalId +
+                ", hospitalName='" + hospitalName + '\'' +
+                ", hospitalAddress='" + hospitalAddress + '\'' +
+                ", contactNo=" + contactNo +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hospital hospital = (Hospital) o;
+        return getHospitalId() == hospital.getHospitalId() && getContactNo() == hospital.getContactNo() && Objects.equals(getHospitalName(), hospital.getHospitalName()) && Objects.equals(getHospitalAddress(), hospital.getHospitalAddress()) && Objects.equals(getEmailId(), hospital.getEmailId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHospitalId(), getHospitalName(), getHospitalAddress(), getContactNo(), getEmailId());
+    }
 }
